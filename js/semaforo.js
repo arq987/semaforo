764eb60e792c6e3cdeef3120e8345167496a3f8f
Alexander Romero Quiroga <arq.987@hotmail.com>
2023-06-19 20:34:07 -0500

    Primer commit del proyecto

---
+++ js/semaforo.js
@@ -0,0 +1,31 @@
+let coloresSemaforo = ['ROJO', 'AMARILLO', 'VERDE'];
+let color = 0;
+let intervalo = null;
+
+function semaforoAutomatico() {
+    let semaforoImagen = document.getElementById('semaforoImagen');
+    let colorSemaforo = coloresSemaforo[color];
+    semaforoImagen.src = "./img/" + colorSemaforo.toLowerCase() + ".png";
+
+    if (colorSemaforo === 'ROJO') {
+        color = 2;
+    } else if (colorSemaforo === 'AMARILLO') {
+        color = 0;
+    } else if (colorSemaforo === 'VERDE') {
+        color = 1;
+    }
+
+}
+
+function iniciarSemaforo() {
+    if (intervalo === null) {
+        semaforoAutomatico();
+        intervalo = setInterval(semaforoAutomatico, 3000);
+    }
+}
+function detenerSemaforo() {
+    clearInterval(intervalo);
+    intervalo = null;
+}
+iniciarSemaforo();
+
